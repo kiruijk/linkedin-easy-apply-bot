7fa7e772a8ccf88db214709376147ec2f1282d85
kiruijk <jameskirui@gmail.com>
2024-06-18 17:48:07 -0400

    customize

--- sample_config.ts
+++ sample_config.ts
@@ -1,56 +1,43 @@
 export default {
   // LOGIN DETAILS
-  LINKEDIN_EMAIL: "",
-  LINKEDIN_PASSWORD: "",
+  LINKEDIN_EMAIL: "${{ secrets.LINKEDIN_USERNAME }}",
+  LINKEDIN_PASSWORD: "${{ secrets.LINKEDIN_PASSWORD }}",
 
   // JOB SEARCH PARAMETERS
-  KEYWORDS: "javascript",
-  LOCATION: "Portugal",
+  KEYWORDS: "DevOps", 
+  LOCATION: "United States",
   WORKPLACE: {
     REMOTE: true,
-    ON_SITE: true,
+    ON_SITE: false,
     HYBRID: false,
   },
   JOB_TITLE: "(javascript|frontend|front-end|fullstack|full-stack|nodejs|node|js).*(developer|engineer)",
   JOB_DESCRIPTION: "^((?!(primeit))(.|[\n\r]))*$",
-  JOB_DESCRIPTION_LANGUAGES: ["portuguese", "english"], // replace value with ["any"] to accept all job description laguages
+  JOB_DESCRIPTION_LANGUAGES: ["english"], // replace value with ["any"] to accept all job description laguages
 
   // FORM DATA
-  PHONE: "912345678",
+  PHONE: "2403676644",
   CV_PATH: "",
   COVER_LETTER_PATH: "",
-  HOME_CITY: "Lisbon, Portugal",
+  HOME_CITY: "Frederick, Maryland",
   YEARS_OF_EXPERIENCE: {
-    "angular": 5,
-    "react.js": 6,
-    ".net": 3,
-    "php": 4,
-    "spring": 4,
-    "java": 4,
-    "magento": 5,
-    "node": 5,
-    "javascript": 5,
-    "mongodb": 5,
-    "kubernetes": 5,
-    "CI/CD": 5,
-    "python": 5,
-    "drupal": 5,
-    "sass": 5,
-    "html": 5,
-    "google cloud": 5,
-    "docker": 5,
-    "terraform": 5,
-    "css": 4,
-    "typescript": 6,
-    "webmethods": 5
+    "kubernetes": 4,
+    "CI/CD": 4,
+    "python": 4,
+    "drupal": 4,
+    "sass": 4,
+    "AWS": 4,
+    "docker": 4,
+    "terraform": 4,
+
   },
   LANGUAGE_PROFICIENCY: {
-    "english": "professional",
-    "spanish": "native",
-    "french": "native"
+    "english": "fluent",
+    "swahili": "native",
+
   },
   REQUIRES_VISA_SPONSORSHIP: false,
-  TEXT_FIELDS: { "salary": "60k" },
+  TEXT_FIELDS: { "salary": "100k" },
   BOOLEANS: {
     "bachelhor|bacharelado": true,
     "authorized": true
